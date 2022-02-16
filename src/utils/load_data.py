bf02b67022db6af7e4adce097eea3c62be6b3899
dungxibo123 <votiendung0805@gmail.com>
2022-02-16 09:35:46 +0700

    Folder tree

---
+++ src/utils/load_data.py
@@ -0,0 +1,58 @@
+import networkx as nx
+import dgl
+import torch_geometric as tg
+
+
+
+def load_data(data_type="Cora"):
+    return None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print("Successfully build, no bug at all!")
